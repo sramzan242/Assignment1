a93444225811f7ed628fcee31bc0417d88738b80
sramzan242 <sramzan242@outlook.com>
2023-05-29 15:18:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+gsap.from(".logo", { opacity: 0, duration: 1, delay: 0.5, y: -50 });
+gsap.from("h2", { opacity: 0, duration: 1, delay: 1, y: -50 });
+gsap.from("p", { opacity: 0, duration: 1, delay: 1.5, y: -50 });
+gsap.from(".btn", { opacity: 0, duration: 1, delay: 2, y: -50 });
